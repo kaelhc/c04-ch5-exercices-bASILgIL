6be1ef53bbc4ac737ee600a9f7de537754cdf684
bASILgIL <70984519+bASILgIL@users.noreply.github.com>
2020-09-22 19:05:41 -0400

    update

--- exercice.py
+++ exercice.py
@@ -6,17 +6,33 @@ from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    return 0
+    if number < 0:
+        return number * (-1)
+    return number
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
 
-    return [""]
+    word_list = []
+    for pre in prefixes:
+        word_list.append(pre + suffixe)
+    return word_list
 
+def is_prime_number(number):
+    for i in range(2, number//2):
+        if number % i == 0:
+            return False
+        return True
 
 def prime_integer_summation() -> int:
-    return 0
+    prime = [2, 3, 5]
+    number = 6
+    while len(prime) < 100:
+        if is_prime_number(number):
+            prime.append(number)
+        number += 1
+    return sum(prime)
 
 
 def factorial(number: int) -> int:
